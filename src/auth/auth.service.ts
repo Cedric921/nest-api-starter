686999b964ca0c5a3939a4786413de3ada1159b0
cedric karungu <ckarungu921@gmail.com>
2022-11-17 16:55:25 +0200

    feat: add auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  register() {
+    return { message: 'register' };
+  }
+
+  login() {
+    return { message: 'login' };
+  }
+}
