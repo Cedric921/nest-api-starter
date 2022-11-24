faa015778cf8f423b57c82ca9a71c709d1d3b3bd
cedric karungu <ckarungu921@gmail.com>
2022-11-24 14:14:49 +0200

    test:add test describes

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,11 @@
+import { PrismaService } from './../src/prisma/prisma.service';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 
 describe('--App e2e Test --', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
   /**
    * Before starting tests, we need some config:
@@ -13,17 +15,26 @@ describe('--App e2e Test --', () => {
    * - init our server created
    */
   beforeAll(async () => {
+    /** import of App module */
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+
+    /** create app  */
     app = moduleRef.createNestApplication();
+
+    /** add the global pipe */
     app.useGlobalPipes(
       new ValidationPipe({
         whitelist: true,
       }),
     );
 
+    /** Initialisation of the app */
     await app.init();
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDB();
   });
 
   it.todo('it pass');
@@ -33,5 +44,47 @@ describe('--App e2e Test --', () => {
    */
   afterAll(() => {
     app.close();
+  });
+
+  describe('-Auth- ', () => {
+    describe('Sign Up', () => {
+      ('');
+    });
+
+    describe('Sign In', () => {
+      ('');
+    });
+  });
+
+  describe('-User-', () => {
+    describe('Get me', () => {
+      ('');
+    });
+
+    describe('Edit user', () => {
+      ('');
+    });
+  });
+
+  describe('-Bookmark-', () => {
+    describe('Create bookmark', () => {
+      ('');
+    });
+
+    describe('Get Bookmarks', () => {
+      ('');
+    });
+
+    describe('Get Bookmarks by Id', () => {
+      ('');
+    });
+
+    describe('Edit bookmark', () => {
+      ('');
+    });
+
+    describe('Delete bookmark', () => {
+      ('');
+    });
   });
 });
